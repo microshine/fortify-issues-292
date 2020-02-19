24f6c338e92e0011e66a5789d111ebd704499010
microshine <microshine@mail.ru>
2020-02-19 12:14:42 +0300

    Print slot information

--- index.ts
+++ index.ts
@@ -8,11 +8,21 @@ async function main() {
   token.initialize();
   try {
     const slot = token.getSlots(slotId);
+    const tokenInfo = slot.getToken();
+    
+    console.log("Slot:");
+    console.log(" Library:", lib);
+    console.log(" Handle:", `0x${slot.handle.toString("hex")}`);
+    console.log(" Label:", tokenInfo.label);
+    console.log(" Manufacturer ID:", token.manufacturerID);
+    console.log(" Description:", token.libraryDescription);
+    console.log(" Version:", `v${token.libraryVersion.major}.${token.libraryVersion.minor}`);
+    console.log();
     const pkcs11 = slot.lib;
     const mechanisms = pkcs11.C_GetMechanismList(slot.handle);
     mechanisms.forEach((mechanism, index) => {
       const name = graphene.MechanismEnum[mechanism];
-      console.log(`Mechanism #${index}`);
+      console.log(`Mechanism #${index + 1}`);
       console.log("  Name:", name ?? `Unknown mechanism(0x${mechanism.toString(16)})`);
       try {
         const mechanismInfo = pkcs11.C_GetMechanismInfo(slot.handle, mechanism);
